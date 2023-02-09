c82c02db925a81784fb01cdcf6a8790927985981
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-02-10 05:09:13 +0530

    added stripe checkout and success page

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,7 +4,7 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    console.log(req.body.cartItems);
+    console.log(req.body);
 
     try {
       const params = {
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
             { shipping_rate: 'shr_1MZ5acSJY1Jvt8vm0g8HmBD7' },
             { shipping_rate: 'shr_1MZ5boSJY1Jvt8vm7slt80wM' },
         ],
-        line_items: req.body.cartItems.map((item) => {
+        line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/lxpin17t/production/').replace('-webp', '.webp')
           console.log('IMAGE', newImage)
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
             quantity: item.quantity,
           }
         }),
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       }
 
       // Create Checkout Sessions from body params.
